c4c15dce9e9adf52ece770fe4df21f5d81940e73
DE SPIRLET  Igor <stg3053@brufor.lan>
2024-06-19 11:58:13 +0200

    Day Night Cycle done

--- Assets/Script/Night Day Cycle/DayNightCycle.cs
+++ Assets/Script/Night Day Cycle/DayNightCycle.cs
@@ -2,67 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteAlways]
 public class DayNightCycle : MonoBehaviour
 {
-    [SerializeField] private Light DirectionalLight;
-    [SerializeField] private LightingPreset Preset;
+    public Light sunLight;
+    public NightScreen nightScreen;
+    public float dayDuration = 120f;
+    public float nightDuration = 10f;
 
-    [SerializeField, Range(0, 24)] private float TimeOfDay;
+    private float currentTime = 0f;
+    private bool isDay = true;
 
-    private void Update()
+    void Start()
     {
-        if (Preset == null)
-        {
-            return;
-        }
-
-        if (Application.isPlaying)
-        {
-            TimeOfDay += Time.deltaTime;
-            TimeOfDay %= 24;
-            UpdateLighting(TimeOfDay / 24f);
-        }
-        else
-        {
-            UpdateLighting(TimeOfDay / 24f);
-        }
+        currentTime = 0f;
+        isDay = true;
+        sunLight.transform.rotation = Quaternion.Euler(-10f, 170f, 0f);
     }
 
-    private void UpdateLighting(float timePercent)
+    void Update()
     {
-        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
-        RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
+        if (isDay)
+        {
+            currentTime += Time.deltaTime;
+            float sunRotationX = currentTime / dayDuration * 180f - 10f;
+            sunLight.transform.rotation = Quaternion.Euler(sunRotationX, 170f, 0f);
 
-        if (DirectionalLight != null)
-        {
-            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
-            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) -90f, 170f, 0));
-        }
-    }
-
-    private void OnValidate()
-    {
-        if (DirectionalLight != null)
-        {
-            return;
-        }
-
-        if (RenderSettings.sun != null)
-        {
-            DirectionalLight = RenderSettings.sun;
-        }
-        else
-        {
-            Light[] lights = GameObject.FindObjectsOfType<Light>();
-            foreach(Light light in lights)
+            if (sunRotationX >= 190f)
             {
-                if (light.type == LightType.Directional)
-                {
-                    DirectionalLight = light;
-                    return;
-                }
+                isDay = false;
+                currentTime = 0f;
+                nightScreen.ShowNightScreen(nightDuration);
             }
         }
+    }
+
+    public void StartNewDay()
+    {
+        isDay = true;
+        currentTime = 0f;
+
+        sunLight.transform.rotation = Quaternion.Euler(-10f, 170f, 0f);
     }
 }
